722b6f89b914bdbed871ce316fbd08405c9993bd
Alex Carlin <carlin@ucdavis.edu>
2015-09-17 03:54:30 -0700

    Added iPython notebook

--- control/blow_bubbles.py
+++
@@ -1,7 +0,0 @@
-import andrew 
-
-sbs_96 = { 'rows': 12, 'cols': 8, 'clear_height': 10, 'x-spacing': 9, 'y-spacing': 9 } 
-
-andrew.know( name='plate', a1=(0,0,0), layout=sbs_96 ) 
-andrew.goto( thing='plate', well='0' )
-andrew.asp( 100 ) 
